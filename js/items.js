5920af12f0b83676fe98747bfc50c89cf72593a0
Lalith Kiran Mannur <truancylounge@gmail.com>
2015-10-11 09:34:04 +0400

    Adding items html page

--- js/items.js
+++ js/items.js
@@ -1,8 +1,14 @@
 var app = angular.module('item', ['ui.bootstrap'])
 app.controller('ItemController', ['$scope', '$http', '$modal', function($scope, $http, $modal) {
-    $http.get('http://localhost:8080/Templetree/rest/items/').success(function(data) {
+    var resGet = $http.get('http://localhost:8080/Templetree/rest/items/');
+
+    resGet.success(function(data) {
       $scope.items = data;
     });
+
+    resGet.error(function(data, status, headers, config) {
+        alert( "failure message: " + JSON.stringify({data: data}));
+    });  
 
     $scope.updateItems = function() {
       console.log("Updating items.");
@@ -27,7 +33,7 @@ app.controller('ItemController', ['$scope', '$http', '$modal', function($scope, 
 
     $scope.open = function () {
       //alert("I am an alert box!");
-      console.log("Inside Add Items modal.");
+      console.log("Entering Add Items modal.");
 
       var modalInstance = $modal.open({
         templateUrl: 'addItemsModal.html',
@@ -37,6 +43,30 @@ app.controller('ItemController', ['$scope', '$http', '$modal', function($scope, 
           return $scope.items;
         }
       }
+      });
+    };
+
+    $scope.deleteItem = function(i) {
+      console.log("Entering deleteItem function.");
+      $scope.items = $scope.items.filter(function(item) {
+        return item.barcode !== i.barcode;
+      });
+    };
+
+    $scope.editItemOpen = function(i) {
+      console.log("Entering EditItem modal.");
+
+      var modalInstance = $modal.open({
+        templateUrl: 'editItemsModal.html',
+        controller: 'EditItemModalController',
+        resolve: {
+          updatedItem: function () {
+            return i;
+          },
+          items: function() {
+            return $scope.items;
+          }
+        }
       });
     };
 }]);
@@ -55,4 +85,41 @@ app.controller('ItemModalController', function ($scope, $modalInstance, items) {
       $modalInstance.dismiss('save');
   };
 });
+
+app.controller('EditItemModalController', ['$scope', '$modalInstance', 'updatedItem', 'items', function ($scope, $modalInstance, updatedItem, items) {
+  
+  console.log("Entering EditItem modal Controller");
+  $scope.itemName = updatedItem.itemName;
+  $scope.quantity = updatedItem.quantity;
+  $scope.salesPrice = updatedItem.salesPrice;
+  $scope.purchasePrice = updatedItem.purchasePrice;
+
+  $scope.updatedItem = updatedItem;
+  $scope.items = items;
+
+  
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+
+  $scope.editItem = function() {
+
+      // Looping through items to find out the item to be updated and edit itemName and quantity.
+      for(var index in items) {
+        var item = items[index];
+        for(var property in item) {
+          if (item.hasOwnProperty(property) && property == "barcode" && item[property] == updatedItem.barcode ) {
+            console.log("Updating itemName and quantity for item with barcode: " + item.barcode);
+            item.itemName = $scope.itemName;
+            item.salesPrice = $scope.salesPrice;
+            item.purchasePrice = $scope.purchasePrice;
+            item.quantity = $scope.quantity;        
+          }
+        }
+      }
+
+      $modalInstance.dismiss(updatedItem);
+
+  };
+}]);
 
