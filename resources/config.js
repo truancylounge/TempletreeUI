b67003c54ec021fc88b9c2deae83325cddd44b23
Lalith Kiran Mannur <truancylounge@gmail.com>
2016-01-11 16:04:48 +0530

    Adding charts

--- resources/config.js
+++ resources/config.js
@@ -1,4 +1,5 @@
 uiProperties = {
-	itemlistUploadUrl:  'http://localhost:8080/Templetree/rest/fileservice/upload/itemlist',	
-	invoiceUploadUrl: 'http://localhost:8080/Templetree/rest/fileservice/upload/invoices'	
+
+	itemlistUploadUrl:  'http://ec2-54-169-254-75.ap-southeast-1.compute.amazonaws.com:8080/TempletreeSystem/rest/fileservice/upload/itemlist',	
+	invoiceUploadUrl: 'http://ec2-54-169-254-75.ap-southeast-1.compute.amazonaws.com:8080/TempletreeSystem/rest/fileservice/upload/invoices'	
 }
