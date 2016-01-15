0caee339c84141e4109e74613433ccd2f942112f
Lalith Kiran Mannur <truancylounge@gmail.com>
2016-01-15 09:55:09 +0530

    Local config changes

--- resources/config.js
+++ resources/config.js
@@ -1,5 +1,4 @@
 uiProperties = {
-
-	itemlistUploadUrl:  'http://ec2-54-169-254-75.ap-southeast-1.compute.amazonaws.com:8080/TempletreeSystem/rest/fileservice/upload/itemlist',	
-	invoiceUploadUrl: 'http://ec2-54-169-254-75.ap-southeast-1.compute.amazonaws.com:8080/TempletreeSystem/rest/fileservice/upload/invoices'	
+	itemlistUploadUrl:  'http://localhost:8080/Templetree/rest/fileservice/upload/itemlist',	
+	invoiceUploadUrl: 'http://localhost:8080/Templetree/rest/fileservice/upload/invoices'	
 }
