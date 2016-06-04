6463f687567f61ce6b1e5bbe36ca7b50d0008f04
Lalith Kiran Mannur <truancylounge@gmail.com>
2016-06-04 10:39:50 -0400

    Changing server details

--- resources/config_dev.js
+++ resources/config_dev.js
@@ -1,5 +1,5 @@
 uiProperties = {
 
-	itemlistUploadUrl:  'http://ec2-54-169-254-75.ap-southeast-1.compute.amazonaws.com:8080/TempletreeSystem/rest/fileservice/upload/itemlist',	
-	invoiceUploadUrl: 'http://ec2-54-169-254-75.ap-southeast-1.compute.amazonaws.com:8080/TempletreeSystem/rest/fileservice/upload/invoices'	
+	itemlistUploadUrl:  'http://ec2-52-77-220-46.ap-southeast-1.compute.amazonaws.com:8080/TempletreeSystem/rest/fileservice/upload/itemlist',	
+	invoiceUploadUrl: 'http://ec2-52-77-220-46.ap-southeast-1.compute.amazonaws.com:8080/TempletreeSystem/rest/fileservice/upload/invoices'	
 }
